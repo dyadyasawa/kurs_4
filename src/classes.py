0be33e2716632cf40af6b5e523c0bda7e0e6e061
dyadyasawa <dyadyasawa@gmail.com>
2024-01-20 17:15:54 +0300

    Добавление функционала в src/classes.py.

--- src/classes.py
+++ src/classes.py
@@ -8,12 +8,12 @@ class WorkApi(ABC):
     """ Абстрактный класс для работы с API сайтов с вакансиями. """
 
     @abstractmethod
-    def get_vacancies(self):
+    def get_vacancies(self, prof, area):
         """ Абстрактный метод для подключения к API и получения вакансий. Реализуется в дочерних классах. """
         pass
 
     @abstractmethod
-    def get_choice_vacancies(self):
+    def get_choice_vacancies(self, prof, area):
         """ Абстрактный метод для выборки вакансий по заданным параметрам. Реализуется в дочерних классах. """
         pass
 
@@ -21,13 +21,25 @@ class WorkApi(ABC):
 class HeadHunterApi(WorkApi):
     """ Класс для работы с вакансиями через API сайта hh.ru. """
 
-    def get_vacancies_hh(self):
+    def get_vacancies(self, prof, area):
         """ Метод для подключения к API и получения вакансий с hh.ru. """
-        pass
 
-    def get_choice_vacancies_hh(self):
+        url = 'http://api.hh.ru/vacancies'
+
+        response = requests.get(f"{url}?text=name:!{prof} AND !{area}").json()
+
+        with open('data/hh_vacancies.json', 'w', encoding=('UTF-8')) as file:
+            json.dump(response, file, ensure_ascii=False, indent=4)
+
+        with open('data/hh_vacancies.json', 'r', encoding=('UTF-8')) as file:
+            dict_info = json.load(file)
+        return dict_info
+
+
+    def get_choice_vacancies(self, prof, area):
         """ Метод для выборки вакансий по заданным параметрам с hh.ru. """
-        pass
+
+        dict_vacancies = self.get_vacancies(prof, area)
 
 
 class SuperJobApi(WorkApi):
