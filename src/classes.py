34a117a08c85e38d6b29a629ebe077ed70751d38
dyadyasawa <dyadyasawa@gmail.com>
2024-01-20 16:20:25 +0300

    Создание абстрактного класса для работы с API сайтов с вакансиям в src/classes.py.

--- src/classes.py
+++ src/classes.py
@@ -2,3 +2,16 @@
 import json
 import requests
 from abc import ABC, abstractmethod
+
+
+class WorkApi(ABC):
+    """ Абстрактный класс для работы с API сайтов с вакансиями. """
+
+    @abstractmethod
+    def get_vacancies(self):
+        """ Абстрактный метод для подключения к API и получения вакансий. Реализуется в дочерних классах. """
+        pass
+
+    @abstractmethod
+    def get_choice_vacancies(self):
+        pass
