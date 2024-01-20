11b6bf0c985af22ab6c7e5fcc01da8db16ba89eb
dyadyasawa <dyadyasawa@gmail.com>
2024-01-20 16:36:39 +0300

    Создание классов и методов для работы с API сайтов с вакансиям в src/classes.py.

--- src/classes.py
+++ src/classes.py
@@ -14,4 +14,29 @@ class WorkApi(ABC):
 
     @abstractmethod
     def get_choice_vacancies(self):
+        """ Абстрактный метод для выборки вакансий по заданным параметрам. Реализуется в дочерних классах. """
+        pass
+
+
+class HeadHunterApi(WorkApi):
+    """ Класс для работы с вакансиями через API сайта hh.ru. """
+
+    def get_vacancies_hh(self):
+        """ Метод для подключения к API и получения вакансий с hh.ru. """
+        pass
+
+    def get_choice_vacancies_hh(self):
+        """ Метод для выборки вакансий по заданным параметрам с hh.ru. """
+        pass
+
+
+class SuperJobApi(WorkApi):
+    """ Класс для работы с вакансиями через API сайта sj.ru. """
+
+    def get_vacancies_sj(self):
+        """ Метод для подключения к API и получения вакансий с sj.ru. """
+        pass
+
+    def get_choice_vacancies_sj(self):
+        """ Метод для выборки вакансий по заданным параметрам с sj.ru. """
         pass
