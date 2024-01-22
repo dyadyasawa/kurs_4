e45111b8df617e11e9c278ff97ff58d5b90552f9
dyadyasawa <dyadyasawa@gmail.com>
2024-01-22 13:33:43 +0300

    Добавление функционала в методы get_vacancies и get_choice_vacancies класса HeadHunterApi в src/classes.py.

--- src/classes.py
+++ src/classes.py
@@ -26,21 +26,40 @@ class HeadHunterApi(WorkApi):
 
         url = 'http://api.hh.ru/vacancies'
 
-        response = requests.get(f"{url}?text=name:!{prof} AND !{area}").json()
+        response = requests.get(f"{url}?text=name:{prof} AND {area}").json()
 
-        with open('data/hh_vacancies.json', 'w', encoding=('UTF-8')) as file:
+        with open('../data/hh_vacancies.json', 'w', encoding=('UTF-8')) as file:
             json.dump(response, file, ensure_ascii=False, indent=4)
 
-        with open('data/hh_vacancies.json', 'r', encoding=('UTF-8')) as file:
+        with open('../data/hh_vacancies.json', 'r', encoding=('UTF-8')) as file:
             dict_info = json.load(file)
-        return dict_info
+        return dict_info['items']
 
 
     def get_choice_vacancies(self, prof, area):
         """ Метод для выборки вакансий по заданным параметрам с hh.ru. """
 
-        dict_vacancies = self.get_vacancies(prof, area)
+        list_vacancies = self.get_vacancies(prof, area)
+        vacancies = []
+        for vacancy in list_vacancies:
+            if vacancy['salary'] == None:
+                continue
 
+            elif vacancy['salary']['from'] == None:
+                continue
+
+            elif vacancy['salary']['to'] == None:
+                continue
+
+            else:
+                vacancies.append({
+                    'name': vacancy['name'],
+                    'salary_from ': vacancy['salary']['from'],
+                    'salary_to': vacancy['salary']['to'],
+                    'description': vacancy['snippet']['responsibility'],
+                    'url': vacancy['employer']['alternate_url']
+                })
+        return vacancies
 
 class SuperJobApi(WorkApi):
     """ Класс для работы с вакансиями через API сайта sj.ru. """
@@ -52,3 +71,7 @@ class SuperJobApi(WorkApi):
     def get_choice_vacancies_sj(self):
         """ Метод для выборки вакансий по заданным параметрам с sj.ru. """
         pass
+
+
+h1 = HeadHunterApi()
+print(h1.get_choice_vacancies('швея',  'краснодар'))
