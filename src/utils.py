3773ad01c6fadc4faf5ef54bc67e02a7a4f07090
dyadyasawa <dyadyasawa@gmail.com>
2024-01-27 13:03:11 +0300

    Исправления в src/utils.py.

--- src/utils.py
+++ src/utils.py
@@ -11,17 +11,18 @@ def interaction_with_user():
     while True:
         letter = input("С какой платформы желаете получить данные? (h - hh.ru  s - superjob.ru  q - выход) >>>  ")
         if letter.lower() == 'h':
-            profession = input("По какой профессии вывести вакансии?: ")
+            profession_hh = input("По какой профессии вывести вакансии?: ")
             print()
             hh = HeadHunterApi()
             save_json_hh = SaveToJson()
 
             switch_hh = 0  # Переключатель для выхода из бесконечного цикла.
             while switch_hh == 0:
-                save_json_hh.write_vacancies_to_json(hh.get_choice_vacancies(profession))
+                save_json_hh.write_vacancies_to_json(hh.get_choice_vacancies(profession_hh))
                 list_vacancies = save_json_hh.read_vacancies_from_json()
                 if len(list_vacancies) == 0:
                     print("Увы, по заданной профессии вакансий не найдено. Попробуйте ввести другие данные.")
+                    HeadHunterApi.page = 1
                     switch_hh = 1
                 else:
                     for ex in sorted(list_vacancies, reverse=True):
@@ -29,7 +30,7 @@ def interaction_with_user():
 
                     print()
                     print(f"Вакансии отсортированы по зарплате, от большей к меньшей.\n"
-                          f"Была выведена страница {HeadHunterApi.page} из {hh.get_vacancies(profession)[1]}.\n")
+                          f"Была выведена страница {HeadHunterApi.page} из {hh.get_vacancies(profession_hh)[1]}.\n")
 
                     switch1_hh = 0  # Переключатель для выхода из бесконечного цикла.
                     while switch1_hh == 0:
@@ -41,7 +42,7 @@ def interaction_with_user():
                         elif value.isdigit() is not True:
                             print("Введите корректное значение.")
 
-                        elif int(value) > hh.get_vacancies(profession)[1]:
+                        elif int(value) > hh.get_vacancies(profession_hh)[1]:
                             print("Введите корректное значение.")
 
                         else:
@@ -49,17 +50,18 @@ def interaction_with_user():
                             switch1_hh = 1
 
         elif letter.lower() == 's':
-            profession = input("По какой профессии вывести вакансии?: ")
+            profession_sj = input("По какой профессии вывести вакансии?: ")
             print()
             sj = SuperJobApi()
             save_json_sj = SaveToJson()
 
             switch_sj = 0  # Переключатель для выхода из бесконечного цикла.
             while switch_sj == 0:
-                save_json_sj.write_vacancies_to_json(sj.get_choice_vacancies(profession))
+                save_json_sj.write_vacancies_to_json(sj.get_choice_vacancies(profession_sj))
                 list_vacancies = save_json_sj.read_vacancies_from_json()
                 if len(list_vacancies) == 0:
                     print("Увы, по заданной профессии вакансий не найдено. Попробуйте ввести другие данные.")
+                    SuperJobApi.page = 1
                     switch_sj = 1
                 else:
                     for ex in sorted(list_vacancies, reverse=True):
@@ -67,7 +69,7 @@ def interaction_with_user():
 
                     print()
                     print(f"Вакансии отсортированы по зарплате, от большей к меньшей.\n"
-                          f"Была выведена страница {SuperJobApi.page} из {count_of_pages(sj.get_vacancies(profession)[1])}.\n")
+                          f"Была выведена страница {SuperJobApi.page} из {count_of_pages(sj.get_vacancies(profession_sj)[1])}.\n")
 
                     switch1_sj = 0  # Переключатель для выхода из бесконечного цикла.
                     while switch1_sj == 0:
@@ -79,7 +81,7 @@ def interaction_with_user():
                         elif value.isdigit() is not True:
                             print("Введите корректное значение.")
 
-                        elif int(value) > count_of_pages(sj.get_vacancies(profession)[1]):
+                        elif int(value) > count_of_pages(sj.get_vacancies(profession_sj)[1]):
                             print("Введите корректное значение.")
 
                         else:
@@ -97,8 +99,8 @@ def count_of_pages(count_of_vacancies):
 
     if count_of_vacancies / 40 < 1:
         pages = 1
-    elif count_of_vacancies / 40 >= 500:
-        pages = 500
+    elif count_of_vacancies / 40 >= 13:
+        pages = 13
     elif count_of_vacancies % 40 == 0:
         pages = int(count_of_vacancies / 40)
     else:
